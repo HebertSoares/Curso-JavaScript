b97cbad3e921c8a77bc186dce1cd5cb2a1a53fa6
HebertSoares <hebert.hbs16@gmail.com>
2021-07-22 22:08:26 -0300

    Update app.js

--- App3/app.js
+++ App3/app.js
@@ -1,155 +1,165 @@
 class Despesa {
-    constructor(ano, mes, dia, tipo, descricao, valor){
-        this.ano = ano
-        this.mes = mes
-        this.dia = dia
-        this.tipo = tipo
-        this.descricao = descricao
-        this.valor = valor
-    }
-    validarDados(){
-        for (let i in this){
-            if(this[i] == undefined || this[i] == '' || this[i] == null){
-                return false
-            }
-        }
-        return true
-    }
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+
+	validarDados() {
+		for(let i in this) {
+			if(this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false
+			}
+		}
+		return true
+	}
 }
 
 class Bd {
 
-    constructor(){
-        let id = localStorage.getItem('id')
+	constructor() {
+		let id = localStorage.getItem('id')
 
-        if(id === null){
-            localStorage.setItem('id', 0)
-        }
-    }
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
 
-    getProximoId() {
-        let proximoId = localStorage.getItem('id') //null nenhum tem id if/else
-        return parseInt(proximoId) + 1
-    }
-    gravar (d){       
-       let id = this.getProximoId()
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
 
-       localStorage.setItem(id, JSON.stringify(d))
+	gravar(d) {
+		let id = this.getProximoId()
 
-       localStorage.setItem('id', id)
-    }
-    recuperarTodosRegistros(){
+		localStorage.setItem(id, JSON.stringify(d))
 
-        // array de despesas
-        let despesas = Array()
+		localStorage.setItem('id', id)
+	}
 
-        let id = localStorage.getItem('id')
+	recuperarTodosRegistros() {
 
-        //recuperar todas as despesas cadastradas em localStorage
-        for (let i = 1; i <= id; i++){
+		//array de despesas
+		let despesas = Array()
 
-            //recuperar a despesa
-            let despesa = JSON.parse(localStorage.getItem(i)) 
-            
-            //existe a possibilidade de haver indices que foram pulados/removidos
-            //neste caso nós vamos pular esses indices
-            if(despesa === null){
-                continue
-            }
+		let id = localStorage.getItem('id')
 
-            despesas.push(despesa)
-        }
+		//recuperar todas as despesas cadastradas em localStorage
+		for(let i = 1; i <= id; i++) {
 
-        return despesas
-    }
+			//recuperar a despesa
+			let despesa = JSON.parse(localStorage.getItem(i))
+
+			//existe a possibilidade de haver índices que foram pulados/removidos
+			//nestes casos nós vamos pular esses índices
+			if(despesa === null) {
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+
+		return despesas
+	}
 }
 
-let bd = new Bd ()
+let bd = new Bd()
 
-function cadastrarDespesa(){
-        
-    let ano = document.getElementById('ano')
-    let mes = document.getElementById('mes')
-    let dia = document.getElementById('dia')
-    let tipo = document.getElementById('tipo')
-    let descricao = document.getElementById('descricao')
-    let valor = document.getElementById('valor')
 
-   
-    let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+function cadastrarDespesa() {
 
-    if(despesa.validarDados()){
-        bd.gravar(despesa)
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
 
-        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
-        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
-        document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
-        document.getElementById('modal_btn').innerHTML = 'Voltar'
-        document.getElementById('modal_btn').className = 'btn btn-success'
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
 
-        //dialog de sucesso    
-        $('#modalRegistraDespesa').modal('show')
-    }else {
 
-        document.getElementById('modal_titulo').innerHTML = 'Erro da inclusão de registro'
-        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
-        document.getElementById('modal_conteudo').innerHTML = 'erro na gravação, verifique se todos os campos foram preenchidos corretamente.'
-        document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
-        document.getElementById('modal_btn').className = 'btn btn-danger'
+	if(despesa.validarDados()) {
+		bd.gravar(despesa)
 
-        //dialog de erro
-        $('#modalRegistraDespesa').modal('show')
-    }
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
+		//dialog de sucesso
+		$('#modalRegistraDespesa').modal('show') 
+		
+
+	} else {
+		
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
+		//dialog de erro
+		$('#modalRegistraDespesa').modal('show') 
+	}
 }
 
-function carregaListaDespesas(){
+function carregaListaDespesas() {
 
-    let despesas = Array()
+	let despesas = Array()
 
-    despesas = bd.recuperarTodosRegistros()
+	despesas = bd.recuperarTodosRegistros() 
 
-    //selecionando o elemento tbody da tabela
-    let listaDespesas = document.getElementById('listaDespesas')
+	/*
 
-    /*
-    <tr>
-        0 = <td>15/03/2018</td>
-        1 = <td>alimentação</td>
-            <td>Compras do mês</td>
-            <tD>444.75</td>
-    </tr>
-    */
+	<tr>
+		<td>15/03/2018</td>
+		<td>Alimentação</td>
+		<td>Compras do mês</td>
+		<td>444.75</td>
+	</tr>
 
-      //percorrer o array despesas, listando cada despesa de forma dinamica
-      despesas.forEach(function(d){
+	*/
 
-        //console.log(d)
+	let listaDespesas = document.getElementById("listaDespesas")
 
-        //criando a linha (tr)
-        let linha = listaDespesas.insertRow()
+	despesas.forEach(function(d){
 
-      //criar as colunas (td)
-      linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
-      
+		//Criando a linha (tr)
+		var linha = listaDespesas.insertRow();
 
-      //ajustar o tipo
-      switch(d.tipo){
-        case '1': d.tipo = 'Alimentação'
-            break
-        case '2': d.tipo = 'Educação'
-            break 
-        case '3': d.tipo = 'Lazer'
-            break
-        case '4': d.tipo = 'Saúde'
-            break
-        case '5': d.tipo = 'Transporte'
-            break
-            
-      }
+		//Criando as colunas (td)
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}` 
 
-      linha.insertCell(1).innerHTML = d.tipo
+		//Ajustar o tipo
+		switch(d.tipo){
+			case '1': d.tipo = 'Alimentação'
+				break
+			case '2': d.tipo = 'Educação'
+				break
+			case '3': d.tipo = 'Lazer'
+				break
+			case '4': d.tipo = 'Saúde'
+				break
+			case '5': d.tipo = 'Transporte'
+				break
+			
+		}
+		linha.insertCell(1).innerHTML = d.tipo
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+		console.log(d)
+	})
 
-      linha.insertCell(2).innerHTML = d.descricao
-      linha.insertCell(3).innerHTML = d.valor
-    })
-}
+ }
